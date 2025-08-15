7eb979d22519f9471ca082b33d7e56c06c8c7f0f
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-08-15 09:42:01 +0700

    Bai 36 Chuong 5

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,21 @@
 const TodoNew = (props) => {
-    console.log(">>> check props: ", props);
-    const { addNewToDo } = props
-    return (
-        <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
-        </div>
-    )
-    
-}
-export default TodoNew
+  
+  const { addNewToDo } = props;
+
+  const handleClick = () => {
+    alert("click me");
+  };
+  const handleOnChange = (name) => {
+    console.log(">>>> text change", name);
+  }
+
+  return (
+    <div className="todo-new">
+      <input type="text" onChange={(event) => {handleOnChange(event.target.value)}} />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
+};
+export default TodoNew;
