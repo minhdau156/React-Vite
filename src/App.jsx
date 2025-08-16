db9655a5762d1d16cd80f19c70970331b8f7e511
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-08-16 21:02:51 +0700

    Bai 40 Chuong 5

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,32 @@ import TodoData from "./components/todo/TodoData";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 const App = () => {
-
-  const [todoList, setTodoList] = useState([
-    {id: 1, name: "Learing React"},
-    {id: 2, name: "Watching Youtube"}
-  ]);
-
-  const hoidanit = "Eric";
-  const data = {
-    country: "VIetNam",
-    address: "BinhDinh"
-  }
+  const [todoList, setTodoList] = useState([]);
 
   const addNewToDo = (name) => {
     const newTodo = {
       id: randomIntFromInterval(1, 1000000),
-      name: name
-    }
+      name: name,
+    };
     setTodoList([...todoList, newTodo]);
-  }
+  };
 
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  };
 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew addNewToDo={addNewToDo}/>
-      <TodoData name={hoidanit} data={data}  todoList={todoList}/>
-      <div className="todo-image">
-        <img src={reactLogo} className="logo" />
-      </div>
+      <TodoNew addNewToDo={addNewToDo} />
+
+      {todoList.length > 0 ? (
+        <TodoData todoList={todoList} />
+      ) : (
+        <div className="todo-image">
+          <img src={reactLogo} className="logo" />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -4,7 +4,7 @@ const TodoData = (props) => {
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {
-        return (<div className="todo-item">
+        return (<div className={`todo-item`} key={item.id}>
                   <div>{item.name}</div>
                   <button>Delete</button>
                 </div>
