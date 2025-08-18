00f66cb1192b8fbecbc4adf32841612fdfa11e62
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-08-18 20:40:26 +0700

    Bai 50 Chuong 6

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,20 +1,18 @@
+import { Link, NavLink } from "react-router-dom";
 import "./header.css";
 const Header = () => {
   return (
-    
-      <ul>
-        <li>
-          <a className="active" href="/">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/users">Users</a>
-        </li>
-        <li>
-          <a href="/products">Products</a>
-        </li>
-      </ul>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/users">Users</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products">Products</NavLink>
+      </li>
+    </ul>
   );
 };
 
